595d979670a1382a5b5ad38228f90c87d61580da
MAYINUDDIN <mayindiu@gmail.com>
2023-07-20 23:15:16 +0600

    first commit

---
+++ src/views/examples/Dark.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Dark = () => {
+    return (
+        <div>
+         
+            
+            <div class="row m-2">
+              
+            <div class="col-md-6 bg-danger m-2">
+            <h2 class="text-white">mayin</h2>
+                
+              </div>
+                    
+            <div class="col-md-6 bg-success m-2">
+            <h2 class="text-white">mayin</h2>
+
+              </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Dark;
