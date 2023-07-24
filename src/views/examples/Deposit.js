d37db2f35e9368cc919c7fe6d4b5c749c535521f
MAYINUDDIN <mayindiu@gmail.com>
2023-07-24 19:56:29 +0600

    All in one

--- src/views/examples/Deposit.js
+++ src/views/examples/Deposit.js
@@ -15,30 +15,50 @@ import {
     Col,
     Input,
     Label,
-} from "reactstrap";
+  } from "reactstrap";
 const Deposit = () => {
     return (
         <div >
-            <div className="container-fluid header bg-gradient-info pb-7 pt-5 pt-md-8">
-                <h2 className='text-white font-weight-bold'>Fund Your <span className='text-white font-weight-bold'>Account Balance</span> </h2>
-            </div>
-
+        <div className="container-fluid header bg-gradient-info pb-7 pt-5 pt-md-8">
+        <h2 className='text-white font-weight-bold'>Fund Your <span className='text-white font-weight-bold'>Account Balance</span> </h2>
+        </div>
+ 
             <Row className='container-fluid'>
+                
+              <Col lg="12" xl="8" className=' mt--7'>
+                <Card className="card-stats shadow-lg shadow-sm--hover  mb-4 mb-xl-0 ">
+                  <CardBody>
+                  <Label for="exampleEmail">
+                   Enter Amount
+                  </Label>
+                  <Input
+                    id="amount"
+                    name="amount"
+                    placeholder="Enter Amount"
+                    type='number'
+                    min='5'
+                    />
+  
+                   <Label className='mt-3' for="exampleEmail">
+                   Choose Payment Method from the list below
+                  </Label>
 
-                <Col lg="12" xl="8" className=' mt--7'>
-                    <Card className="card-stats shadow-lg shadow-sm--hover  mb-4 mb-xl-0 ">
+                   <Row>
+                   <Col lg="12" xl="6" className=' mt-3'>
+                        <Card className="shadow-lg   mb-4 mb-xl-0 ">
                         <CardBody>
-                            <Label for="exampleEmail">
-                                Enter Amount
-                            </Label>
-                            <Input
-                                id="amount"
-                                name="amount"
-                                placeholder="Enter Amount"
-                                type='number'
-                                min='5'
-                            />
-  <CardBody>
+                        <p>BUSD</p>
+                        </CardBody>
+                        </Card>
+                    </Col>
+                    <Col lg="12" xl="6" className=' mt-3'>
+                        <Card className="shadow-lg  mb-4 mb-xl-0 ">
+                        <CardBody>
+                        <p>USDT</p>
+                        </CardBody>
+                        </Card>
+                    </Col>
+
                    </Row>
                    <Row>
                    <Col lg="12" xl="6" className=' mt-3'>
