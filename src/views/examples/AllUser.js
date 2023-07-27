9dd5dfb6ee8fe740e3b679de84cd547f0832c16f
alamin_saifur <saifurrahman1614@gmail.com>
2023-07-27 15:15:52 +0600

    status er kaj

--- src/views/examples/AllUser.js
+++ src/views/examples/AllUser.js
@@ -1,14 +1,37 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { Table } from "reactstrap";
 
 const AllUser = () => {
   const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState(null);
+  console.log(message)
   useEffect(() => {
     fetch("https://indian.munihaelectronics.com/public/api/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
-  console.log(users)
+  // console.log(users)
+ 
+  const handleActive = async (id)=>{
+    console.log(id)
+
+    
+    try {
+      const response = await axios.put(
+        `https://indian.munihaelectronics.com/public/api/update_status/${id}`,
+        
+      );
+        console.log(response?.data?.message)
+      setMessage(response?.data?.message)
+      
+    } catch (error) {
+      console.log(error?.response);
+      toast.error(error.response?.data?.error);
+    }
+  };
+  
   return (
     <div>
       <div className="container-fluid header bg-gradient-info pb-2 pt-5 pt-md-8">
@@ -31,13 +54,17 @@ const AllUser = () => {
                 <td>{user?.name}</td>
                 <td>{user?.email}</td>
                 <td>
-                  {
-                    user?.status == 0  ? <button className="btn btn-success border-none">
-                    Active
-                  </button> : <button className="btn btn-danger border-none">
-                    Diactive
-                  </button>
-                  }
+                  {/* <button  onClick={()=>handleActive(user?.id)}>Button</button> */}
+                {
+                  message === 'User Active SuccessFull' ?
+                  <button className="btn btn-success"
+                  onClick={()=>handleActive(user?.id)}
+                  >Active</button> :
+                  message === 'User Deactive SuccessFull' ?
+                  <button className="btn btn-danger"
+                  onClick={()=>handleActive(user?.id)}
+                  >DeActive</button> : ''
+                }
                 </td>
               </tr>
             ))}
